7f223039fed1da8fd327d7baffd5998fee557d71
potejj <potej.org@gmail.com>
2024-12-09 00:05:32 +0100

    coss1

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function calculation(){
+    
+let coffe = 15
+let watter = 200
+let bloom = true
+let bloom_ratio = 3
+let ratio = 0
+
+    if(bloom){
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='data'>
+       <input type='text' placeholder="{coffe}" className='coffe'/> 
+       <input type='text' placeholder="{watter}" className='watter'/> 
+       <input type='text' placeholder="{bloom_ratio}" className='bloom'/> 
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
+    }
+    else{
+    }
 }
-
 export default App
